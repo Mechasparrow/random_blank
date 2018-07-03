399b1520939dfd465ebe42eb232e8f563b240d15
Michael Navazhylau <mikipux7@gmail.com>
2018-07-03 15:52:34 -0500

    application does its job

--- src/my_random_blank_gen/core.clj
+++ src/my_random_blank_gen/core.clj
@@ -6,14 +6,19 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (do
+  (let [end-application (atom false)]
+   (while (= @end-application false) (do
     (println "Welcome to my random blank")
-    (println "What would you like to do for random choice? (create/read)")
+    (println "What would you like to do for random choice? (create/read/exit)")
     (def ans (read-line))
-
+    (if (= ans "exit") (do
+                         (swap! end-application (fn [atm] true))
+                         (println "exiting..."))
+        (do 
     (def res-list (case ans
       "read" (prompts/list-read-prompt)
-      "create" (prompts/create-list-prompts)))
+      "create" (prompts/create-list-prompts)
+      nil))
     (def rand-item (generator/random-item res-list))
-    (println (str "the random item is " rand-item))))
+    (println (str "the random item is " rand-item))))))))
     
